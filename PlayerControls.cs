fc43d5f10c130a888eb6d893e857c3658d59e41c
kangsudal_notebook <kangsudal@gmail.com>
2022-07-07 23:15:35 +0900

    74. New Input system

--- PlayerControls.cs
+++ PlayerControls.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
     // Start is called before the first frame update
+
+    private void OnEnable() {
+        movement.Enable();
+    }
+
+    private void OnDisable() {
+        movement.Disable();
+    }
+    
     void Start()
     {
         
@@ -13,8 +24,10 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        float verticalThrow = Input.GetAxis("Vertical");
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+        // float horizontalThrow = Input.GetAxis("Horizontal");
+        // float verticalThrow = Input.GetAxis("Vertical");
         Debug.Log("x:"+horizontalThrow+",y:"+verticalThrow);
     }
 }
